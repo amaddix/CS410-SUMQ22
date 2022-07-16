104f8d2f471b6a3698eadf91ff7e6f4c723b94df
amaddix <amaddix@pdx.edu>
2022-07-15 21:22:44 -0700

    resubmitting

--- phonebill/src/main/java/edu/pdx/cs410J/amaddix/PhoneCall.java
+++ phonebill/src/main/java/edu/pdx/cs410J/amaddix/PhoneCall.java
@@ -18,18 +18,33 @@ public class PhoneCall extends AbstractPhoneCall {
     this.endTime=null;
   }
 
+  /**
+   * constructor initializing :
+   * @param tcaller
+   * @param tcallee
+   * @param tstartTime
+   * @param tendTime
+   */
 
   //constructor with caller and callee
-  public PhoneCall(String tcaller, String tcallee){
+  public PhoneCall(String tcaller, String tcallee, String tstartTime, String tendTime){
     this.caller=tcaller;
     this.callee=tcallee;
+    this.startTime=tstartTime;
+    this.endTime=tendTime;
   }
 
+  /**
+   * constructor copying a phone call
+   * @param tempcall
+   */
 
   //constructor with 2 string num
   public PhoneCall(PhoneCall tempcall){
     this.caller= tempcall.caller;
     this.callee=tempcall.callee;
+    this.startTime=tempcall.startTime;
+    this.endTime= tempcall.endTime;
   }
 
   //constructor with a string name and a call
@@ -45,26 +60,40 @@ public class PhoneCall extends AbstractPhoneCall {
    */
 
 
+  /**
+   * @return caller
+   */
   @Override
   public String getCaller() {
     return this.caller;
   //  throw new UnsupportedOperationException("This method is not implemented yet");
   }
 
+  /**
+   * @return callee
+   */
   @Override
   public String getCallee() {
     return this.callee;
     //return "This method is not implemented yet";
   }
 
+  /**
+   * @return start time
+   */
   @Override
   public String getBeginTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.startTime;
   }
+
+  /**
+   * @return endTime
+   */
 
   @Override
   public String getEndTimeString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return this.endTime;
+   // throw new UnsupportedOperationException("This method is not implemented yet");
   }
 }
 
--- phonebill/src/main/resources/edu/pdx/cs410J/amaddix/README.txt
+++ phonebill/src/main/resources/edu/pdx/cs410J/amaddix/README.txt
@@ -3,10 +3,6 @@ This is a README file!
 Ashley Maddix
 CS410-Summer 2022 - JavaP
 
-**Disclosure- I think I started this assignment a little to close to the due date/ it took longer that expected to get
-java/JDK reinstalled properly, and for the class files to install and work properly. So My project doesnt seem to
-test very well**
-
 This project currently reads in 3 strings from the command line, a customer name, caller phone number, and a callee
 phone number. The main program reads in these args and confirms that the first arg is a string of only charactors,
 and confirms both the following args are strings containing 10 digits (to match a phone number). If these requirements
